db2510ce38cb2492f55a4a1a26f8ddb7f8d8ce37
Gkbdc01 <142567524+Gkbdc01@users.noreply.github.com>
2023-09-03 20:24:53 +0530

    it calculate the volume of cylinder with radius and  height given by user .

---
+++ volume_cylinder.py
@@ -0,0 +1,7 @@
+# to calculate the volume of t he cylinder with given radius and height.
+r = int(input("Enter the radius of cylinder :"))
+h = int(input("Enter the height of the cylinder : "))
+import math as g
+l = g.pi
+v = l*r**2*h
+print("The volume of the cylinder is :",v)
