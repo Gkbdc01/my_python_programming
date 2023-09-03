c706c30381b927c29c43d9dc635d91ab91e87976
Gkbdc01 <142567524+Gkbdc01@users.noreply.github.com>
2023-09-03 20:23:15 +0530

    it will calculate the volume of the sphere with  radius given by user.

---
+++ volume_sphere.py
@@ -0,0 +1,6 @@
+#to calculate the volume of sphere with given radius. 
+r = int(input("Enter he radius of sphere"))
+import math
+c = math.pi
+v = 4/3*c*r**3
+print("the volume of the sphere is :",v)
