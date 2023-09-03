eaf2a0151ba36edec0954e7f3bcbaaa1260c972d
Gkbdc01 <142567524+Gkbdc01@users.noreply.github.com>
2023-09-03 20:36:23 +0530

    it will calculate the  sum of even  numbers till which the user will give.

---
+++ sum_of_even_numbers.py
@@ -0,0 +1,5 @@
+# to calculte the sum of even number til user say.
+a = int(input("enter the number till which you want the even sum :"))
+n = a//2
+d = n*(n+1)
+print("the sum of even numbers will be :",d)
